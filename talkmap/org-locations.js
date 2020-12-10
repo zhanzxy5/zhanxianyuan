5e38bac635e946b2d4008ea47490a107b1bc897a
zhanxianyuan <zhanxianyuan@jd.com>
2020-12-10 10:00:11 +0800

    geocode talks

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -1,22 +1,52 @@
 var addressPoints = [
   [
-    "Berkeley CA, USA",
-    37.8708393,
-    -122.2728638
+    "Minneapolis, Minnesota",
+    44.9772995,
+    -93.2654692
   ],
   [
-    "London, UK",
-    51.5073219,
-    -0.1276473
+    "New York, US",
+    40.7127281,
+    -74.0060152
   ],
   [
-    "San Francisco, California",
-    37.7792808,
-    -122.4192362
+    "Rutgers University, New Brunswick, New Jersey",
+    40.500075949999996,
+    -74.4457915242934
   ],
   [
-    "Los Angeles, CA",
-    34.0543942,
-    -118.2439408
+    "Philadelphia, US",
+    39.9527237,
+    -75.1635262
+  ],
+  [
+    "Nashville, US",
+    36.1622296,
+    -86.7743531
+  ],
+  [
+    "Purdue University, West Lafayette, Indiana",
+    40.430067050000005,
+    -86.92478793273786
+  ],
+  [
+    "Chicago, Illinois",
+    41.8755616,
+    -87.6244212
+  ],
+  [
+    "Beijing, China",
+    39.9020803,
+    116.7185213
+  ],
+  [
+    "Hangzhou, China",
+    30.2489634,
+    120.2052342
+  ],
+  [
+    "University of Chinese Academy of Sciences, Beijing, China",
+    39.9082804,
+    116.2458527
   ]
 ];
